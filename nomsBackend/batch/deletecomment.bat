985724c3682b77a0646822c074805246bc6e92fe
Mark Kropivnitski <theultimatesizzle@gmail.com>
2015-06-22 13:47:31 -0700

    second commit

---
+++ nomsBackend/batch/deletecomment.bat
@@ -0,0 +1,13 @@
+@echo off
+set /p username="What is the username of the person whose comment you want to delete?\n"
+echo "\n"
+set /p comment="What is the beginning of the offending comment?\n"
+echo "\n"
+
+curl -X POST \
+-H "X-Parse-Application-Id: lp7KkFHgqW9yVe17Zjfm1K5Txmq5AK7JuYcDSHkP" \
+  -H "X-Parse-REST-API-Key: eJmb8UryS07m7MwDOP2WrN5aoC5mUiSPzbHZRqpH" \
+  -H "Content-Type: application/json" \
+  -G \
+  -d '{"user":%username%,"commentText":%comment%}' \
+  https://api.parse.com/1/functions/deleteComment
--- nomsBackend/cloud/main.js
+++ nomsBackend/cloud/main.js
@@ -10,9 +10,9 @@ function increment(request,object,member) {
 	return function(request) {
 		query = new Parse.Query(object);
 		query.get(request.object.get(object).id, {
-			success: function(object) {
-				object.increment(member);
-				object.save();
+			success: function(obj) {
+				obj.increment(member);
+				obj.save();
 			},
 			error: function(error) {
 				console.error(Error on comment save, error.code + " : " + error.message);
@@ -23,13 +23,13 @@ function increment(request,object,member) {
 
 Parse.Cloud.afterSave("Comment", increment(request,"nom","commentCount"));
 
-Parse.Cloud.afterSave("nom", increment(request,Parse.User,"nomCount"));
+Parse.Cloud.afterSave("nom", increment(request,"publicUser","nomCount"));
 
 //nom likes will have to be incremented from the front end
 
 Parse.Cloud.define("follow", function(request) {
-	var follower = request.User;
-	var followee = request.params.User;
+	var follower = request.User.get("is");
+	var followee = request.params.User.get("is");
 	
 	var toRelation = follower.relation("following");
 	var fromRelation = followee.relation("followedBy");
@@ -39,7 +39,7 @@ Parse.Cloud.define("follow", function(request) {
 
 	followee.increment("followingCount");
 	follower.increment("followerCount");
-}
+});
 
 //Validation
 
@@ -56,7 +56,70 @@ function checkInvalidString(request,response,member) {
 
 Parse.Cloud.beforeSave("Comment", checkInvalidString(request,response,"text"));
 Parse.Cloud.beforeSave(Parse.User,checkInvalidString(request,response,"username"));
+Parse.Cloud.beforeSave("publicUser",checkInvalidString(request,response,"username"));
 Parse.Cloud.beforeSave("Deal", checkInvalidString(request,response,"dealString"));
 Parse.Cloud.beforeSave("nom", checkInvalidString(request,response,"Title"));
 Parse.Cloud.beforeSave("nom", checkInvalidString(request,response,"Description"));
 Parse.Cloud.beforeSave("Restaurant", checkInvalidString(request,response,"Name"));
+
+// Public user creation
+
+
+
+Parse.Cloud.beforeSave(Parse.User, function(request, response) {
+	var publicUser = Parse.Object.extend("publicUser");
+	publicUser.set("username",request.user.get("username"));
+	publicUser.set("is", request.user);
+	publicUser.save(null,null,null);
+});
+
+//Temporary Moderation utilities
+
+function findUser(username) {
+	var user;
+	var publicUser = Parse.Object.extend("publicUser");	
+	var userQuery = Parse.Query(publicUser);
+	userQuery.equalTo("username",username);
+	userQuery.first({
+		success: function(theUser) {
+			user = theUser;
+		}
+	});
+	return user;
+}
+
+Parse.Cloud.define("deleteComment",function(request,response,user,commentText){
+	var offendingUser = findUser(user);
+	var offendingComment;
+	
+	var comment = Parse.Object.extend("comment");
+	var query = Parse.Query(comment);
+	query.equalTo("author",offendingUser);
+	query.startsWith("text",commentText);
+		
+	query.first({
+		success: function(theComment) {
+			offendingComment = theComment;
+		}
+	});
+	
+	offendingComment.destroy({});
+});
+
+Parse.Cloud.define("deleteNom",function(request,response,user,nomTitle){
+	var offendingUser = findUser(user);
+	var offendingNom;
+	
+	var nom = Parse.Object.extend("nom");
+	var query = Parse.Query(nom);
+	query.equalTo("createdBy",offendingUser);
+	query.startsWith("title",nomTitle);
+		
+	query.first({
+		success: function(theNom) {
+			offendingNom = theNom;
+		}
+	});
+	
+	offendingNom.destroy({});
+});
